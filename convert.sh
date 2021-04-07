9f108c52e1604e9bdcd07788fb778fe076b7c62b
huhao <huhao@huhaos-MacBook-Pro.local>
2021-04-08 01:08:28 +0800

    .

---
+++ convert.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")/output;FILE=$DIR/x.png;
+
+rm -fr $FILE
+
+osascript -e "get the clipboard as «class PNGf»" | sed "s/«data PNGf//; s/»//" | xxd -r -p > $FILE
+
+# brew install imagemagick  
+convert $FILE -resize 50% $FILE
+
+open $DIR
